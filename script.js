3d43c5830447e21691d7bdf1bfe8a9730f60ed0f
salva1510 <hjpoffice2@gmail.com>
2026-02-13 12:49:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -427,4 +427,15 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+// =============================
+// SCROLL PROGRESS FUNCTION
+// =============================
+
+window.addEventListener("scroll", function () {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    document.getElementById("scrollProgress").style.width = scrollPercent + "%";
+});
                                                                                                                                                                                                                   
